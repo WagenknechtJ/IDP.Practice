94ec490fabeb4a356870a454c12ef82f8b141eeb
donutjess <stellae.jbk@gmail.com>
2021-01-01 22:43:01 -0600

    Now with graphs! Ooooh, ahhhhh

--- IDP.try1.py
+++ IDP.try1.py
@@ -1,12 +1,12 @@
 import json
-import pprint
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 #file = open('C:\\Users\\koprekj\\github\\PracticeandLearning\\DP00613.json',)
 file = open('C:\\Users\\koprekj\\github\\IDP.Practice\\GeneFilePP.json',)
 
 #seaborn's distplot function??
-
-#USING JUST JSON AND PYTHON
 
 data = json.load(file)
 
@@ -22,33 +22,37 @@ for i in data['data']:
         IDs.update({i['disprot_id']: i['sequence'][start: end]})
         ScrollingSeq += i['sequence'][start: end]
 
-#pprint.pprint(IDs)
-#print(ScrollingSeq)
-
 totalAA = 0
-AADict = dict.fromkeys(['ACount', 'RCount', 'NCount', 'DCount', 'CCount', 'QCount', 'ECount', 'GCount', 'HCount', 'ICount', 'LCount', 'KCount', 'MCount', 'FCount', 'PCount', 'SCount', 'TCount', 'WCount', 'YCount', 'VCount'])
+AADict = dict.fromkeys(['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'])
 
 for j in ScrollingSeq:
     totalAA += 1
 
 print("\n\nTotal AA's = \t" + str(totalAA))
 
+Aminos = {}
 for key in AADict:
     counts = 0
     stringed = str(key)
-    seeking = stringed[0]
     for j in ScrollingSeq:
-        if j == seeking:
+        if j == stringed:
             counts += 1
     AADict[key] = counts
     fraction = round(int(AADict[key])/totalAA, 3)
-    print("Total " + seeking + " =\t" + str(counts) + "\tFraction of AA's that are " + seeking + " = \t\t" + str(fraction))
+    print("Total " + stringed + " =\t" + str(counts) + "\tFraction of AA's that are " + stringed + " = \t\t" + str(fraction))
 
-basics = int(AADict['RCount']) + int(AADict['HCount']) + int(AADict['KCount'])
+basics = int(AADict['R']) + int(AADict['H']) + int(AADict['K'])
 print("\nBasic AA's = \t" + str(basics) + "\tFraction of AA's that are basic = \t" + str(round(basics/totalAA, 3)))
-acidics = int(AADict['DCount']) + int(AADict['ECount'])
+acidics = int(AADict['D']) + int(AADict['E'])
 print("Acidic AA's = \t" + str(acidics) + "\tFraction of AA's that are acidic = \t" + str(round(acidics/totalAA, 3)))
 
+plt.bar(dict.keys(AADict), dict.values(AADict))
+plt.title("Amino Acid Distribution of Caenorhabditis elegans")
+plt.xlabel("Amino Acid")
+plt.ylabel("How many occurances")
+plt.show()
 
+# WHATS NEXT - find average AA distribution in proteins in roundworm, compare to AA distribution here
+# look further into other areas of difference or comparison
 
 file.close()
